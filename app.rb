4e7a36942219ff119279113a9f9e6cbb0c703e9c
LeonorPDX <leonoracolbert@gmail.com>
2020-04-16 11:10:00 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Leonor"
+      end
+
+      get '/hometown' do
+        "My hometown is Portland"
+      end
+
+      get '/favorite-song' do
+        "My favorite song is 'Toes' by Glass Animals"
+      end
 end
